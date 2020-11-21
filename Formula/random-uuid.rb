8f5b62572db3cc5c04c0b51ead9f476a5918f5fd
Prasanna Boga <prasannaboga@gmail.com>
2020-11-21 18:44:34 +0530

    Fixed -v command option

--- Formula/random-uuid.rb
+++ Formula/random-uuid.rb
@@ -1,11 +1,13 @@
+# rubocop:disable  Naming/FileName
 class RandomUuid < Formula
   desc "Generate a random UUID"
   homepage "https://github.com/prasannaboga/homebrew-random-uuid"
-  version "0.0.2"
+  version "0.0.3"
 
-  url "https://github.com/prasannaboga/homebrew-random-uuid/archive/0.0.2.zip", using: :curl
+  url "https://github.com/prasannaboga/homebrew-random-uuid/archive/#{version}.zip", using: :curl
 
   def install
     bin.install "bin/random-uuid"
   end
 end
+# rubocop:enable  Naming/FileName
