7d0403bd7b2715df3f70f4fa909177579fd0d552
Prasanna Boga <prasannaboga@gmail.com>
2020-11-21 18:22:02 +0530

    Updated version 0.0.2

--- Formula/random-uuid.rb
+++ Formula/random-uuid.rb
@@ -1,7 +1,7 @@
 class RandomUuid < Formula
   desc "Generate a random UUID"
   homepage "https://github.com/prasannaboga/homebrew-random-uuid"
-  version "0.0.1"
+  version "0.0.2"
 
   url "https://github.com/prasannaboga/homebrew-random-uuid/archive/main.zip", using: :curl
 
